f46da181009373176adea0e08ed8d4bdff6e796f
Andrew Deck <ajd4223@rit.edu>
2017-02-27 11:13:10 -0500

    Add tables to the system models

--- knockserver/models.py
+++ knockserver/models.py
@@ -3,7 +3,31 @@ from sqlalchemy.orm import relationship
 from knockserver.database import Base
 
 class ExampleObject(Base):
-    __tablename__ = 'example_objects'
+    __tablename__ = 'example_object'
     id = Column(BigInteger().with_variant(Integer, "sqlite"), primary_key=True)
     example_string = Column(String)
 
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(BigInteger().with_variant(Integer, "sqlite"), primary_key=True)
+    hashedPass = Column(String)
+    salt = Column(String)
+    username = Column(String)
+
+class AccessPattern(Base):
+    __tablename__ = 'accesspattern'
+    id = Column(BigInteger().with_variant(Integer, "sqlite"), primary_key=True)
+    active = Column(Boolean)
+    expiration = Column(BigInteger().with_variant(Integer, "sqlite"))
+    maxUses = Column(BigInteger().with_variant(Integer, "sqlite"))
+    name = Column(String)
+    usedCount = Column(BigInteger().with_variant(Integer, "sqlite"))
+    children = relationship("patternpiece", backref="accesspattern")
+
+class PatternPiece(Base):
+    __tablename__ = 'patternpiece'
+    id = Column(BigInteger().with_variant(Integer, "sqlite"), primary_key=True)
+    length = Column(BigInteger().with_variant(Integer, "sqlite"))
+    order = Column(BigInteger().with_variant(Integer, "sqlite"))
+    patternID = Column(Integer, ForeignKey('accesspattern.id'))
+    pressed = Column(Boolean)
