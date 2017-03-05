79fb0eaf8025bad759aa113768443aa86ef6cf16
Matthew Crawford <mcrawford0126@gmail.com>
2017-03-05 17:46:40 -0500

    add button

---
+++ knockserver/static/script.js
@@ -0,0 +1,12 @@
+var recording = false;
+
+
+function send_status() {
+    $.post({
+        url: "",
+        data : { is_recording: !recording},
+        success : function(json) {
+            console.log(data.is_recording);
+        }
+    })
+}
