0e2d5d216083a6c9179f9d828135948d9270346a
Matthew Crawford <mcrawford0126@gmail.com>
2017-03-07 23:15:38 -0500

    add ability to grab unix time from datepicker

--- knockserver/static/script.js
+++ knockserver/static/script.js
@@ -2,15 +2,20 @@ var recording = false;
 
 
 function send_status() {
+
+    var dateUnixTime = $('#datetimepicker').data("DateTimePicker").date().unix();
+
     $.post({
         url: "",
-        data : { is_recording: !recording},
+        data : { is_recording: !recording, time: dateUnixTime},
         success : function(json) {
             console.log(data.is_recording);
         }
-    })
+    });
+
+    recording = true;
 }
 
 $(function () {
-    $('#datetimepicker1').datetimepicker();
+    $('#datetimepicker').datetimepicker();
 });
