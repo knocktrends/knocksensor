26028ee3fb091f9361e99d44376616059dbbd067
Matthew Crawford <mcrawford0126@gmail.com>
2017-03-07 23:42:04 -0500

    add an input field for number of uses

--- knockserver/static/script.js
+++ knockserver/static/script.js
@@ -4,14 +4,17 @@ var recording = false;
 function send_status() {
 
     var dateUnixTime = $('#datetimepicker').data("DateTimePicker").date().unix();
+    var uses = $("#uses").val();
+    console.log(dateUnixTime)
+    console.log(uses);
 
-    $.post({
-        url: "",
-        data : { is_recording: !recording, time: dateUnixTime},
-        success : function(json) {
-            console.log(data.is_recording);
-        }
-    });
+    // $.post({
+    //     url: "",
+    //     data : { is_recording: !recording, time: dateUnixTime},
+    //     success : function(json) {
+    //         console.log(data.is_recording);
+    //     }
+    // });
 
     recording = true;
 }
