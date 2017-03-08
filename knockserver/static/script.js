912cf9c271267fdeda0af05dac4b19d3033b726b
Matthew Crawford <mcrawford0126@gmail.com>
2017-03-08 12:45:02 -0500

    fix ajax request to post to /patterns/

--- knockserver/static/script.js
+++ knockserver/static/script.js
@@ -5,14 +5,15 @@ function send_status() {
 
     var dateUnixTime = $('#datetimepicker').data("DateTimePicker").date().unix();
     var uses = $("#uses").val();
-    console.log(dateUnixTime);
 
-    $.post({
+    var date = dateUnixTime.toString();
+
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        dataType: 'json',
         url: "http://127.0.0.1:5000/patterns/",
-        data : { name: "Test", expiration: dateUnixTime, maxUses: uses},
-        success : function(json) {
-            console.log(json);
-        }
+        data : JSON.stringify({name: "Test", expiration: date, maxUses: uses})
     });
 
     recording = true;
