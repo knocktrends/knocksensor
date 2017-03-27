bda8d3425e5823669f24c85bead8b9511c092eb5
Corban Mailloux <github@corb.co>
2017-03-27 12:25:50 -0400

    Add another test and DB connection.

--- test_matching.py
+++ test_matching.py
@@ -1,8 +1,10 @@
 import os
-from knockserver import database, app
+from knockserver import database, app, models
 import unittest
 import tempfile
 import json
+
+from knockserver.database import db_session
 
 
 class KnockServerTestCase(unittest.TestCase):
@@ -15,6 +17,7 @@ class KnockServerTestCase(unittest.TestCase):
             database.init_db()
 
     def tearDown(self):
+        db_session.remove()
         os.close(self.db_fd)
         os.unlink(app.config['DATABASE'])
 
@@ -26,16 +29,42 @@ class KnockServerTestCase(unittest.TestCase):
             follow_redirects=True
         )
 
-        print(resp.data)
         data = json.loads(resp.data)
         self.assertEqual(data['success'], False)
+
+    def test_perfect_match(self):
+        stored_pattern = models.AccessPattern()
+        stored_pattern.active = True
+        stored_pattern.name = "Test Pattern 1"
+        stored_pattern.pending = False
+        stored_pattern.used_count = 0
+
+        for i in range(3):
+            piece = models.PatternPiece()
+            piece.length = 123
+            piece.order = i
+            # First int is pressed time so all even indexed values are pressed
+            piece.pressed = i % 2 == 0
+
+            stored_pattern.pattern_pieces.append(piece)
+
+        db_session.add(stored_pattern)
+        db_session.commit()
+
+        resp = self.app.post(
+            '/knock/',
+            data=json.dumps({"pattern": [123, 123, 123]}),
+            content_type='application/json',
+            follow_redirects=True
+        )
+
+        print(resp.data)
+        data = json.loads(resp.data)
+        self.assertEqual(data['success'], True)
 
     # - Can ignore an empty button press (THis should never happen but we should check for this)
     # - Can detect if the length of two button press patterns are different.
     # - Can detect if the length of two button press patterns are the same.
-
-    # def test_
-
 
 
 if __name__ == '__main__':
