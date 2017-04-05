e3d597fee7ced9119ab6de02ee376f9b78b04b3b
kmb4577 <kmb4577@gmail.rit.edu>
2017-04-05 14:13:51 -0400

    Created the function for testing to see if a notification is in do not disturb mode. Returns true if it is in dnd mode and false if it is not.

--- knockserver/views.py
+++ knockserver/views.py
@@ -162,3 +162,16 @@ def send_failure_notification(device_identifier):
         for notification in NotificationPrefrences.query.filter(NotificationPreference.id == profile.id).all():
             if(notification.failed_attempts_threshold >= 0 and device.failure_count % notification.failed_attempts_threshold == 0):
                 requests.get('https://maker.ifttt.com/trigger/{notification.failure_endpoint}/with/key/{user.ifttt_secret}')
+
+##
+# Subtracts 5670 minutes (4 hours) to get
+# into eastern time-zone.
+#
+def in_dnd_mode(notification):
+    utc_seconds_today = time.time() % 86400
+    edt_seconds_today = utc_seconds_today - 14400
+    minutes_today = edt_seconds_today/60
+    if((minutes_today < notification.send_no_earlier) || (minutes_today > notification.send_no_later)):
+        return true
+    else:
+        return false
