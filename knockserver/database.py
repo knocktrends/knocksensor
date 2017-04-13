ed6a2907f275fa4649ade9f4aa1af198d8dfc760
Jake Darling <jake.darling1@gmail.com>
2017-04-13 08:09:31 -0400

    Add support for unit testing with different database
    config.TESTING must be set to True before running unit tests

--- knockserver/database.py
+++ knockserver/database.py
@@ -6,6 +6,9 @@ import os
 
 # Get the database string from config
 db_connect_string = config.DATABASE_URI
+if config.TESTING:
+    db_connect_string = config.TEST_DATABASE_URI
+
 
 engine = create_engine(db_connect_string, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
@@ -17,10 +20,13 @@ db_session = scoped_session(sessionmaker(autocommit=False,
 Base = declarative_base()
 Base.query = db_session.query_property()
 
-
 def init_db():
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
     import knockserver.models
     Base.metadata.create_all(bind=engine)
+
+if config.TESTING:
+    os.remove(config.TEST_DATABASE_URI[10:]) # strip 'sqlite:///'
+    init_db()
